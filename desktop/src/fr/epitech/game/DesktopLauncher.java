db0534fd2fba180dba74d7270c759f04a9033ff9
Bryan <bryan.ferrando@epitech.eu>
2023-11-07 15:07:10 +0000

    First commit

---
+++ desktop/src/fr/epitech/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package fr.epitech.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import fr.epitech.game.EpiGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("EpiGame");
+		new Lwjgl3Application(new EpiGame(), config);
+	}
+}
