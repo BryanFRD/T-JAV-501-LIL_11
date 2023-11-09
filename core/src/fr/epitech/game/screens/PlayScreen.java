747d662be8903ad7b94a720ba1939325a73a7222
Bryan <bryan.ferrando@epitech.eu>
2023-11-09 19:29:17 +0000

    Added tmx world

--- core/src/fr/epitech/game/screens/PlayScreen.java
+++ core/src/fr/epitech/game/screens/PlayScreen.java
@@ -8,7 +8,12 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -22,26 +27,54 @@ public class PlayScreen implements Screen {
     private final OrthographicCamera camera;
     private final Viewport viewport;
     private final Hud hud;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private World world;
+    private Box2DDebugRenderer b2dr;
 
     public PlayScreen(EpiGame game){
-        new Sprite(new Texture("badlogic.jpg"));
         this.game = game;
         this.camera = new OrthographicCamera();
         this.viewport = new FitViewport(EpiGame.V_WIDTH, EpiGame.V_HEIGHT, camera);
         this.hud = new Hud(game.getBatch());
+        this.mapLoader = new TmxMapLoader();
+        this.map = mapLoader.load("test.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+        world = new World(new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer();
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+    }
+
+    public void handleInput(float delta){
+        if(Gdx.input.isTouched()){
+            camera.position.x += 100 * delta;
+        }
     }
 
     @Override
     public void show() {
+    }
 
+    public void update(float delta){
+        handleInput(delta);
+        camera.update();
+        renderer.setView(camera);
     }
 
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.getBatch().setProjectionMatrix(hud.getStage().getCamera().combined);
         hud.getStage().draw();
+        renderer.render();
     }
 
     @Override
