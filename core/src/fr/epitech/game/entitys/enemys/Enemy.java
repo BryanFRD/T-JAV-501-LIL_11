03e925eb88abad152d926b63b31091b90f19a965
simon marchal <simon.marchal@epitech.eu>
2023-11-09 05:28:13 -0500

    update enemy

--- core/src/fr/epitech/game/entitys/enemys/Enemy.java
+++ core/src/fr/epitech/game/entitys/enemys/Enemy.java
@@ -1,16 +1,15 @@
-package fr.epitech.game.Entity.enemy;
+package fr.epitech.game.entitys.enemys;
 
 import com.badlogic.gdx.math.Vector2;
-import fr.epitech.game.Entity.MovableEntity.MovableEntity;
+import fr.epitech.game.entitys.movablesEntitys.MovableEntity;
 
 
 public abstract class Enemy extends MovableEntity {
-    public Enemy(String name, Vector2 coordinate) {
+    public Enemy(String name, Vector2 coordinate, Integer health, Float speed) {
         super(name, coordinate);
     }
 
     public void moveTo(float x, float y){
         super.moveTo(x, y);
     }
-
 }
