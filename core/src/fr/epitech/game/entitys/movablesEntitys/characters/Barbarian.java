631c03470fcc1ba7ee1ebee5664e97750f12f4ab
Pierre Rochette <pierre.rochette@epitech.eu>
2023-11-10 14:54:25 +0100

    new entity

---
+++ core/src/fr/epitech/game/entitys/movablesEntitys/characters/Barbarian.java
@@ -0,0 +1,7 @@
+package fr.epitech.game.entitys.movablesEntitys.characters;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
